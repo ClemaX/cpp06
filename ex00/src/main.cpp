5cde46400201859d9caf81d1260a5caee7df2caf
Clément Hamada <clementhamada@protonmail.com>
2021-02-28 23:35:54 +0100

    ex00: Force floating point notation

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,35 +1,54 @@
 #include <strtold.hpp>
 #include <convert.hpp>
+#include <iomanip>
 
 static void	convertString(std::string const& str)
 {
-	const char	c = *str.begin();
-	long double	value;
+	std::streamsize const	precision = std::cout.precision();
+	char const				c = *str.begin();
+	long double				value;
+	bool					isRound;
 
 	if (str.length() == 1 && std::isprint(c) && !std::isdigit(c))
+	{
 		value = static_cast<long double>(c);
+		isRound = true;
+	}
 	else
 	{
 		std::string::const_iterator	endIt;
 
 		value = strtold(str, endIt);
-
 		if (endIt != str.end())
 		{
 			std::cout << "invalid input: " << str << std::endl;
 			return;
 		}
+		isRound = std::fmod(value, 1.0) == 0.0;
 	}
+
+	if (isRound)
+		std::cout << std::fixed << std::setprecision(1);
+
 	std::cout << "char: ";
 	convertChar(value);
+
 	std::cout << std::endl << "int: ";
 	convert<int>(value);
+
 	std::cout << std::endl << "float: ";
 	if (convert<float>(value))
 		std::cout << 'f';
+
 	std::cout << std::endl << "double: ";
 	convert<double>(value);
 	std::cout << std::endl;
+
+	if (isRound)
+	{
+		std::cout << std::setprecision(precision);
+		std::cout.unsetf(std::ios_base::floatfield);
+	}
 }
 
 int			main(int ac, const char** av)
